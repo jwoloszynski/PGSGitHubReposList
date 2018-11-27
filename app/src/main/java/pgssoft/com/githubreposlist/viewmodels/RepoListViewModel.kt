890a23f23780614b195efc4da9ea670f296c0653
jwoloszynski <jwoloszynski@pgs-soft.com>
2018-11-27 13:44:43 +0100

    merged RepoFetcher to RepoRepository

--- app/src/main/java/pgssoft/com/githubreposlist/viewmodels/RepoListViewModel.kt
+++ app/src/main/java/pgssoft/com/githubreposlist/viewmodels/RepoListViewModel.kt
@@ -6,14 +6,13 @@ import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
 import pgssoft.com.githubreposlist.PGSRepoApp
 import pgssoft.com.githubreposlist.data.RepoRepository
-import pgssoft.com.githubreposlist.data.ReposFetcher
 import pgssoft.com.githubreposlist.data.db.Repository
 import pgssoft.com.githubreposlist.utils.PrefsHelper
 
 class RepoListViewModel : ViewModel() {
 
 
-    private val repoRepository = RepoRepository
+    private val repoRepository = RepoRepository()
 
     private var repoListLiveData: LiveData<List<Repository>>
     private var repoListCount: LiveData<Int>
@@ -23,7 +22,7 @@ class RepoListViewModel : ViewModel() {
         get() = _repoListErrorText
 
     private lateinit var prefs: PrefsHelper
-    private val fetcher = ReposFetcher()
+
 
     init {
         repoListLiveData = repoRepository.getRepoList()
@@ -42,13 +41,12 @@ class RepoListViewModel : ViewModel() {
 
     }
 
-
     fun onRefresh(itemCount: Int) {
         prefs = PrefsHelper(PGSRepoApp.app)
         val timeRefreshed = prefs.time
         val timeBetween = System.currentTimeMillis() - timeRefreshed
         if ((timeRefreshed == -1L) or (timeBetween > (1 * 60 * 1000)) or (itemCount < 1)) {
-            fetcher.fetchAll { error -> _repoListErrorText.value = error }
+            repoRepository.fetchAll { error -> _repoListErrorText.value = error }
             prefs.time = System.currentTimeMillis()
         } else {
             _repoListErrorText.value = null
@@ -58,10 +56,9 @@ class RepoListViewModel : ViewModel() {
 
     fun clearRepoList() {
 
-        fetcher.clearRepoList()
+        repoRepository.clearRepoList()
 
     }
-
 
     private fun getCountText(count: Int): String {
 
