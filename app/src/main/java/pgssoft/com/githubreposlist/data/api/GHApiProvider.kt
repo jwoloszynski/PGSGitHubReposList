827f913d6ffa61a14a9e22fb52df5d30af8519aa
jwoloszynski <jwoloszynski@pgs-soft.com>
2018-12-17 15:45:49 +0100

    okhttp cache

--- app/src/main/java/pgssoft/com/githubreposlist/data/api/GHApiProvider.kt
+++ app/src/main/java/pgssoft/com/githubreposlist/data/api/GHApiProvider.kt
@@ -1,6 +1,8 @@
 package pgssoft.com.githubreposlist.data.api
 
+import okhttp3.Cache
 import okhttp3.OkHttpClient
+import pgssoft.com.githubreposlist.PGSRepoApp
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -10,6 +12,7 @@ object GHApiProvider {
 
         private val client: OkHttpClient =
             OkHttpClient.Builder().connectTimeout(5, TimeUnit.SECONDS).writeTimeout(20, TimeUnit.SECONDS)
+                .cache(Cache(PGSRepoApp.app.cacheDir, 10*1024*1024))
                 .readTimeout(30, TimeUnit.SECONDS).build()
 
         fun getApi(): GHApi {
